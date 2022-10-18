73be4bd5992ce0143eb16608ee05881520c34850
Trent Gamblin <766383+goob256@users.noreply.github.com>
2022-10-18 06:00:47 -0600

    It compiles as 32 bit now

--- misc/batch_files/cmake/windows/tgui6.bat
+++ misc/batch_files/cmake/windows/tgui6.bat
@@ -1,1 +1,1 @@
-cmake .. -DUSER_INCLUDE_PATH="C:\Users\trent\code\include\SDL2;C:\Users\trent\code\include" -DUSER_LIBRARY_PATH=C:\Users\trent\code\lib -G "Visual Studio 17 2022" -DCMAKE_SYSTEM_VERSION=7 %*
+cmake .. -A Win32 -DUSER_INCLUDE_PATH="C:\Users\trent\code\include\SDL2;C:\Users\trent\code\include" -DUSER_LIBRARY_PATH=C:\Users\trent\code\lib -G "Visual Studio 16 2019" -DCMAKE_SYSTEM_VERSION=7 %*
